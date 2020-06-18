7ab1c1621fef1ee70666d77d1f8b7c8f93ab319f
vagrant <vagrant@homestead>
2020-06-18 02:27:10 +0000

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $YES = 'SI';
+
+        if ($YES == 'SI') {
+            echo 'si es igual';
+        }
+
+        return view('home');
+    }
+}
