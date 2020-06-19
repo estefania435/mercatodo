7e21cdc0dda40d881f302e74d97724f6c952609f
Estefania Agudelo <estefaniaagudelo@MacBook-Pro-de-estefania.local>
2020-06-19 17:23:02 -0500

    feat:email verification to complete registration

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
+
+
+
+
