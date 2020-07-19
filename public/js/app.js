50671169f72559d298db43bba1cca3d4f8f8577b
Estefania Agudelo <estefa.valencia21@gmail.com>
2020-07-18 22:50:12 -0500

    feat: an exclusive vue file is created for adminlte

--- public/js/app.js
+++ public/js/app.js
@@ -49669,10 +49669,10 @@ module.exports = function(module) {
 
 /***/ }),
 
-/***/ "./resources/js/apicategory.js":
-/*!*************************************!*\
-  !*** ./resources/js/apicategory.js ***!
-  \*************************************/
+/***/ "./resources/js/admin/apicategory.js":
+/*!*******************************************!*\
+  !*** ./resources/js/admin/apicategory.js ***!
+  \*******************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -49760,84 +49760,9 @@ var apicategory = new Vue({
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-__webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
+__webpack_require__(/*! ./store/bootstrap */ "./resources/js/store/bootstrap.js");
 
-window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', __webpack_require__(/*! ./components/ExampleComponent.vue */ "./resources/js/components/ExampleComponent.vue")["default"]);
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-if (document.getElementById('app')) {
-  var app = new Vue({
-    el: '#app'
-  });
-}
-
-if (document.getElementById('apicategory')) {
-  __webpack_require__(/*! ./apicategory */ "./resources/js/apicategory.js");
-}
-
-if (document.getElementById('confirmdelete')) {
-  __webpack_require__(/*! ./confirmdelete */ "./resources/js/confirmdelete.js");
-}
-
-/***/ }),
-
-/***/ "./resources/js/bootstrap.js":
-/*!***********************************!*\
-  !*** ./resources/js/bootstrap.js ***!
-  \***********************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-  window.Popper = __webpack_require__(/*! popper.js */ "./node_modules/popper.js/dist/esm/popper.js")["default"];
-  window.$ = window.jQuery = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
-
-  __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.js");
-} catch (e) {}
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-
-window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-// import Echo from 'laravel-echo';
-// window.Pusher = require('pusher-js');
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+__webpack_require__(/*! ./comun */ "./resources/js/comun.js");
 
 /***/ }),
 
@@ -49910,6 +49835,37 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/js/comun.js":
+/*!*******************************!*\
+  !*** ./resources/js/comun.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
+Vue.component('example-component', __webpack_require__(/*! ./components/ExampleComponent.vue */ "./resources/js/components/ExampleComponent.vue")["default"]);
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+if (document.getElementById('app')) {
+  var app = new Vue({
+    el: '#app'
+  });
+}
+
+if (document.getElementById('apicategory')) {
+  __webpack_require__(/*! ./admin/apicategory */ "./resources/js/admin/apicategory.js");
+}
+
+if (document.getElementById('confirmdelete')) {
+  __webpack_require__(/*! ./confirmdelete */ "./resources/js/confirmdelete.js");
+}
+
+/***/ }),
+
 /***/ "./resources/js/confirmdelete.js":
 /*!***************************************!*\
   !*** ./resources/js/confirmdelete.js ***!
@@ -49929,6 +49885,51 @@ var confirmdelete = new Vue({
     }
   }
 });
+
+/***/ }),
+
+/***/ "./resources/js/store/bootstrap.js":
+/*!*****************************************!*\
+  !*** ./resources/js/store/bootstrap.js ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+try {
+  window.Popper = __webpack_require__(/*! popper.js */ "./node_modules/popper.js/dist/esm/popper.js")["default"];
+  window.$ = window.jQuery = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+
+  __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.js");
+} catch (e) {}
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+
+window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+// import Echo from 'laravel-echo';
+// window.Pusher = require('pusher-js');
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     forceTLS: true
+// });
 
 /***/ }),
 
--- resources/views/plantilla/admin.blade.php
+++ resources/views/plantilla/admin.blade.php
@@ -807,6 +807,6 @@
 <!-- AdminLTE for demo purposes -->
 <script src="/admin-lte/dist/js/demo.js"></script>
 
-<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="{{ asset('js/app_admin.js') }}" defer></script>
 </body>
 </html>
