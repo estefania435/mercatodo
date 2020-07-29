30353150a1c09c8576668bd18e9febbe4c0e068e
Estefania Agudelo <estefa.valencia21@gmail.com>
2020-07-29 11:05:29 -0500

    feat: search functionality is added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,9 +23,14 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-            $products = \App\MercatodoModels\Product::paginate(10);
+        $name = $request->get('name');
+
+        $products = Product::with('images', 'category')
+            ->where('name', 'like', "%$name%")->orderBy('name')->paginate(9);
+
+        //$products = \App\MercatodoModels\Product::paginate(10);
             return view('home', compact('products'));
     }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,25 @@
 
 @section('content')
     <div class="container">
+
+        <!-- SEARCH FORM -->
+        <div class="card-tools">
+            <form>
+
+                <div class="input-group input-group-sm" style="width: 150px;">
+                    <input type="text" name="name" class="form-control float-right"
+                           placeholder="Buscar"
+                           value="{{ request()->get('name') }}"
+                    >
+
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+        <!-- -->
         <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
             <h1 class="display-4">Ofertas</h1>
 
@@ -9,36 +28,39 @@
         </div>
         <!---->
 
-        <!-- -->
-        <div class="container">
-            <div class="row mb-2">
 
-                <div class="col-md-6">
-                    <div
-                        class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="container">
+            <div class="album py-5 bg-light">
+
+                <div class="container">
+
+                    <div class="row">
                         @foreach($products as $item)
                             @if($item->quantity>0 & $item->status=='Offer')
-                        <div class="col p-4 d-flex flex-column position-static">
-                            <h3 class="mb-0">{{ $item->name }}</h3>
-                            <p class="mb-auto">{{ $item->description }}</p>
-                            <p class="card-text">{{ $item->price }}</p>
-                            <div class="btn-group">
-                                <td><a class="btn btn-sm btn-outline-secondary"
-                                       href="{{ route('product.show',$item->id) }}">See</a></td>
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Agregar al
-                                    carrito
-                                </button>
-                            </div>
-                        </div>
-                        <div class="col-auto d-none d-lg-block">
-                            <img class="bd-placeholder-img"
-                                 src="{{ $item->images->random()->url }}"
-                                 width="267" height="225" title="Pets">
-
-
-                        </div>
+                                <div class="col-md-4">
+                                    <div class="card mb-4 shadow-sm">
+                                        <img class="bd-placeholder-img"
+                                             src="{{ $item->images->random()->url }}"
+                                             width="267" height="225" title="Pets">
+                                        <div class="card-body">
+                                            <p class="card-text">{{ $item->name }}</p>
+                                            <p class="card-text">{{ $item->description }}</p>
+                                            <p class="card-text">{{ $item->price }}</p>
+                                            <div class="d-flex justify-content-between align-items-center">
+                                                <div class="btn-group">
+                                                    <td><a class="btn btn-sm btn-outline-secondary"
+                                                           href="{{ route('product.show',$item->id) }}">See</a></td>
+                                                    <button type="button" class="btn btn-sm btn-outline-secondary">Agregar al
+                                                        carrito
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             @endif
                         @endforeach
+
                     </div>
                 </div>
             </div>
@@ -47,6 +69,12 @@
 
             <!---->
             <div class="album py-5 bg-light">
+
+                <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+                    <h1 class="display-4">Productos</h1>
+
+                </div>
+
                 <div class="container">
 
                     <div class="row">
@@ -82,24 +110,7 @@
             <!---->
 
         </div>
-    <!--
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-        <div class="alert alert-success" role="alert">
-{{ session('status') }}
-            </div>
-@endif
-
-        You are logged in!
-    </div>
-</div>
-</div>
-</div>-->
+        </div>
     </div>
 @endsection
 
