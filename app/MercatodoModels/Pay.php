22c56f5002e0910aa1b49624a92dac40def2606d
Estefania Agudelo <estefa.valencia21@gmail.com>
2020-09-17 17:10:03 -0500

    refactor: scope for queries and try catch improvement

--- app/MercatodoModels/Pay.php
+++ app/MercatodoModels/Pay.php
@@ -3,6 +3,8 @@
 namespace App\MercatodoModels;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
 
 class Pay extends Model
 {
@@ -17,5 +19,10 @@ class Pay extends Model
     public function orders()
     {
         return $this->belongsTo("App\MercatodoModels\Order");
+    }
+
+    public function scopepay($query)
+    {
+        return $query->where('user_id', Auth::user()->id)->where('status', '0');
     }
 }
