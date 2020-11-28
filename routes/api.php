14b92613a793c06e5694948d302014752b2f37e6
Estefania Agudelo <estefa.valencia21@gmail.com>
2020-11-27 21:46:45 -0500

    feat: a REST API is created to manage the products

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,15 @@ Route::apiResource('category', 'API\CategoryController')->names('api.category');
 Route::apiResource('product', 'API\ProductController')->names('api.product');
 
 Route::get('/autocomplete', 'API\AutocompleteController@autocomplete')->name('autocomplete');
+
+Route::get('Product', 'API\ProductController@showAllProducts');
+
+Route::get('Product/{slug}', 'API\ProductController@ShowAProduct');
+
+Route::post('Product', 'API\ProductController@createProduct');
+
+Route::delete('Product/{id}', 'API\ProductController@deleteProduct');
+
+Route::put('Product/{id}', 'API\ProductController@updateProduct');
+
+Route::post('Product/{id}', 'API\ProductController@restore');
