6806dd6202b9b87517aab0b2353d60ae65b61b48
Estefania Agudelo <estefa.valencia21@gmail.com>
2020-12-02 10:43:14 -0500

    test: new tests are created for the functionalities of importing and generating product reports

---
+++ tests/Feature/product/ExportTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature\product;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ExportTest extends TestCase
+{
+
+}
